a9e5534d09208d0a0692a311b8da49c99e05d452
plungerman <plungerman@gmail.com>
2020-04-17 10:13:10 -0500

    trying to figure out why pyodbc barfs when DEBUG=False in settings.py but works fine when DEBUG=True

--- djshed/bin/shed.py
+++ djshed/bin/shed.py
@@ -6,7 +6,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djshed.settings')
 
 from django.conf import settings
 
-from djshed.constants import *
+from djshed.constants import SCHEDULE_SQL
 
 from djimix.core.utils import get_connection
 
@@ -35,45 +35,61 @@ parser.add_argument(
     help="Four digit year.",
     dest='year'
 )
+parser.add_argument(
+    '--test',
+    action='store_true',
+    help="Dry run?",
+    dest='test'
+)
 
 
 def main():
-    """
-    Fetch the data and print
-    """
-    weir = '''
-        AND sec_rec.sess = '{}'
-        AND sec_rec.yr = '{}'
-        ORDER BY dept, crs_no, sec_no
-    '''.format(term, year)
-    sql = '{} {}'.format(SCHEDULE_SQL, weir)
-    print(sql)
-    connection = get_connection()
-    cursor = connection.cursor()
-    sched = cursor.execute(sql)
-    if sched:
-        count = 1
-        columns = [column[0]for column in sched.description]
-        results = []
-        for row in sched.fetchall():
-            results.append(dict(zip(columns, row)))
-        for r in results:
-            count += 1
-            print("{}|{}".format(count, r))
+    """Fetch schedule data and print."""
+    if test:
+        sql = """
+            {0} AND  sec_rec.sess[1,1] in ("R","A","G","T","P")
+            ORDER BY sec_rec.yr DESC, program, sec_rec.sess
+        """.format(SCHEDULE_SQL)
     else:
-        print("No schedule data")
+        weir = '''
+            AND sec_rec.sess = '{}'
+            AND sec_rec.yr = '{}'
+            ORDER BY dept, crs_no, sec_no
+        '''.format(term, year)
+        sql = '{} {}'.format(SCHEDULE_SQL, weir)
 
-    # close our database cursor and connection
-    cursor.close()
-    connection.close()
+
+    if test:
+        print(sql)
+    else:
+        connection = get_connection()
+        cursor = connection.cursor()
+        sched = cursor.execute(sql)
+        if sched:
+            count = 1
+            columns = [column[0]for column in sched.description]
+            results = []
+            for row in sched.fetchall():
+                results.append(dict(zip(columns, row)))
+            for r in results:
+                count += 1
+                print("{}|{}".format(count, r))
+        else:
+            print("No schedule data")
+
+        # close our database cursor and connection
+        cursor.close()
+        connection.close()
 
 
 if __name__ == '__main__':
     args = parser.parse_args()
     term = args.term
     year = args.year
+    test = args.test
 
-    print(args)
+    if test:
+        print(args)
 
     sys.exit(main())
 
