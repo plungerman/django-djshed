3926f4c8f2a698fa46b8b7c47a29ac29b5a74d00
plungerman <plungerman@gmail.com>
2021-11-30 13:45:05 -0600

    minor tweaks after migration to app.carthage.edu

--- djshed/wsgi_default.py
+++ djshed/wsgi_default.py
@@ -1,17 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""WSGI configuration."""
+
 import os
-import time
-import traceback
-import signal
 import sys
 
+
 # python
-sys.path.append('/data2/python_venv/2.7/djshed/lib/python2.7/')
-sys.path.append('/data2/python_venv/2.7/djshed/lib/python2.7/site-packages/')
-sys.path.append('/data2/python_venv/2.7/djshed/lib/django_projects/')
-sys.path.append('/data2/python_venv/2.7/djshed/lib/django-djshed/')
+sys.path.append('/data2/python_venv/3.8/djshed/lib/python3.8/')
+sys.path.append('/data2/python_venv/3.8/djshed/lib/python3.8/site-packages/')
 # django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djshed.settings')
-os.environ.setdefault('PYTHON_EGG_CACHE', '')
+os.environ.setdefault('PYTHON_EGG_CACHE', '/var/cache/python/.python-eggs')
 os.environ.setdefault('TZ', 'America/Chicago')
 # informix
 os.environ['INFORMIXSERVER'] = ''
@@ -24,15 +24,4 @@ os.environ['LD_LIBRARY_PATH'] = '$INFORMIXDIR/lib:$INFORMIXDIR/lib/esql:$INFORMI
 os.environ['LD_RUN_PATH'] = '$INFORMIXDIR/lib:$INFORMIXDIR/lib/esql:$INFORMIXDIR/lib/tools:/usr/lib/apache2/modules'
 # wsgi
 from django.core.wsgi import get_wsgi_application
-
-# NOTE: remove the try/except in production
-#application = get_wsgi_application()
-try:
-    application = get_wsgi_application()
-except Exception:
-    # Error loading applications
-    if 'mod_wsgi' in sys.modules:
-        traceback.print_exc()
-        os.kill(os.getpid(), signal.SIGINT)
-        time.sleep(2.5)
-    exit(-1)
+application = get_wsgi_application()
