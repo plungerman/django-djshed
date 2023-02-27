aad662ef82ca7db1286140f2c5cad5026680c3f2
plungerman <plungerman@gmail.com>
2023-02-27 14:47:12 -0600

    added instructors to data model

--- djshed/models.py
+++ djshed/models.py
@@ -24,7 +24,32 @@ class Course(models.Model):
     Data class model for course data.
 
     API JSON structure:
-
+    {
+        "Academic_Units_group": [{"Department": "Political Science"}],
+        "Year": "2023",
+        "Course_Subjects_group": [{"Course_Subject": "POL"}],
+        "Section_Listings_group": [{
+            "Meeting_Time": "9:15 AM - 10:20 AM",
+            "Credits": "4",
+            "Capacity": "12",
+            "Course_Subject_Abbreviation___Number": "POL 2400",
+            "Course_Title": "American Government: National, State, and Local (SOC)(SI)",
+            "Section_Number": "01",
+            "Meeting_Day_Patterns": "MWF"
+        }],
+        "Academic_Period": "2023 Fall",
+        "Course_Description": "<p>4cr This course involves a study of the institutions of American government at the national, state, and local levels and is designed to serve students seeking teacher certification. It will stress the informal as well as the formal dimensions of government and will, and thereby attempt to broaden and deepen insight into the processes of policy-making and implementation. Fall/Spring<\/p>",
+        "Start_Date": "2023-09-06",
+        "End_Date": "2023-12-22",
+        "Instructors_group": [{
+            "Instructor_Name": "Jerald Mast",
+            "Instructor_ID": "491193"
+        }],
+        "Locations_group": [{
+            "Building": "LH",
+            "Room_Number": "230"
+        }]
+    },
     {
         "Academic_Units_group": [{"Department": "Art"}],
         "Year": "2023",
@@ -50,21 +75,22 @@ class Course(models.Model):
     """
 
     title = models.CharField(max_length=255)
-    time = models.CharField(max_length=255, null=True, blank=True)
-    credits = models.CharField(max_length=255)
-    capacity = models.CharField(max_length=255)
-    number = models.CharField(max_length=255)
-    section = models.CharField(max_length=255)
-    days = models.CharField(max_length=255, null=True, blank=True)
-    department = models.CharField(max_length=255)
-    year = models.CharField(max_length=255)
-    group = models.CharField(max_length=255)
-    term = models.CharField(max_length=255)
+    time = models.CharField(max_length=64, null=True, blank=True)
+    credits = models.CharField(max_length=8)
+    capacity = models.CharField(max_length=8)
+    number = models.CharField(max_length=24)
+    section = models.CharField(max_length=8)
+    days = models.CharField(max_length=12, null=True, blank=True)
+    department = models.CharField(max_length=64)
+    year = models.CharField(max_length=4)
+    group = models.CharField(max_length=8)
+    term = models.CharField(max_length=32)
     description = models.TextField(null=True, blank=True)
     start_date = models.DateField("Start Date")
     end_date = models.DateField("End Date")
-    building = models.CharField(max_length=255, null=True, blank=True)
-    room = models.CharField(max_length=255, null=True, blank=True)
+    instructors = models.CharField(max_length=255, null=True, blank=True)
+    building = models.CharField(max_length=32, null=True, blank=True)
+    room = models.CharField(max_length=8, null=True, blank=True)
     status = models.BooleanField(default=True)
 
     def __str__(self):
