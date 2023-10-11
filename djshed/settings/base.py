a2ddbd9815f44edb5c0596434f6c5f0f0227237b
plungerman <plungerman@gmail.com>
2023-10-11 11:31:38 -0500

    cache infrastructure update for django 4.2.5

--- djshed/settings/base.py
+++ djshed/settings/base.py
@@ -121,7 +121,7 @@ TEMPLATES = [
 ]
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
         'LOCATION': '127.0.0.1:11211',
         'TIMEOUT': "60*60*24",
         'KEY_PREFIX': '{0}_'.format(PROJECT_APP),
