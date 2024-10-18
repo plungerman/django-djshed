43662061810dc8e28c66072bb14e687cd75585e1
plungerman <plungerman@gmail.com>
2024-10-18 14:12:36 -0500

    removed informix constants

--- djshed/settings/base.py
+++ djshed/settings/base.py
@@ -4,17 +4,6 @@
 
 import os
 
-#from djimix.settings.local import DBSERVERNAME
-#from djimix.settings.local import INFORMIX_ODBC
-#from djimix.settings.local import INFORMIX_ODBC_TRAIN
-#from djimix.settings.local import INFORMIXDIR
-#from djimix.settings.local import INFORMIXSERVER
-#from djimix.settings.local import INFORMIXSQLHOSTS
-#from djimix.settings.local import LD_LIBRARY_PATH
-#from djimix.settings.local import LD_RUN_PATH
-#from djimix.settings.local import MSSQL_EARL
-#from djimix.settings.local import ODBCINI
-#from djimix.settings.local import ONCONFIG
 
 # Debug
 DEBUG = False
